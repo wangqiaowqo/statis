797570124fed886cf6885d97f91965d752d4746d
wangqiaowqo <qiaowang@qiaowang.(none)>
2013-01-07 15:10:34 +0800

    test

--- src/main/java/com/discoveryLog/callback/LogCallback.java
+++ src/main/java/com/discoveryLog/callback/LogCallback.java
@@ -1,25 +1,27 @@
 package com.discoveryLog.callback;
 
-import org.apache.thrift.async.AsyncMethodCallback; 
+import org.apache.thrift.async.AsyncMethodCallback;
 
-public class LogCallback implements AsyncMethodCallback { 
-   Object response = null; 
+public class LogCallback implements AsyncMethodCallback {
+   Object response = null;
 
-   public Object getResult() { 
+   public Object getResult() {
        // 返回结果值
-       return this.response; 
-   } 
+       return response;
+   }
 
    // 处理服务返回的结果值
-   @Override 
-   public void onComplete(Object response) { 
-       this.response = response; 
-   } 
-   
+   @Override
+   public void onComplete(Object response) {
+       this.response = response;
+   }
+
+
+
    // 处理调用服务过程中出现的异常
 	@Override
 	public void onError(Exception exception) {
 		// TODO Auto-generated method stub
-		
-	} 
-} 
+
+	}
+}
